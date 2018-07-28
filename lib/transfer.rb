44c1e51a0f3db2224d13032fc4b81515958a3719
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-28 16:18:10 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,18 @@
 class Transfer
-  # your code here
+
+  attr_reader :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = 'pending'
+  end
+
+  def valid?
+  end
+
+  def execute_transaction
+  end 
+
 end
